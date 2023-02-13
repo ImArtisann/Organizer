7495f136a3e1a9320eb4492e12089b97017c765d
ImArtisann <jwaggerby1@gmail.com>
2023-02-13 16:42:37 -0600

    No longer hard coded

--- CallOrganizer/src/main/java/org/example/chatListener.java
+++ CallOrganizer/src/main/java/org/example/chatListener.java
@@ -9,59 +9,63 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.utils.FileUpload;
 
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class chatListener extends ListenerAdapter {
 
+    public String guild = "1066874960779431976";
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        //Change ID to lukes this is mine
-        if(event.getAuthor().getId().equals("895670016165433355")){
-            String[] ta = event.getMessage().getContentDisplay().split(" ");
-            if(ta[0].equalsIgnoreCase("+ta")){
-                List<Message.Attachment> image = event.getMessage().getAttachments();
-                if(!image.isEmpty()){
-                    String messageID = event.getMessageId();
-                    CompletableFuture<File> future = image.get(0).downloadToFile( "ta.png");
-                    future.thenAccept(file -> {
-                        event.getJDA().getGuildById("1066874960779431976").getTextChannelById("1073397470421319772")
-                                .sendMessage(event.getMessage().getContentDisplay()).addFile(new File("ta.png")).queue();
-                    });
-                    future.exceptionally(error -> { // handle possible errors
-                        error.printStackTrace();
-                        return null;
-                    });
 
-                }else{
-                    event.getJDA().getGuildById("1066874960779431976").getTextChannelById("1073397470421319772")
-                            .sendMessage(event.getMessage().getContentDisplay()).queue();
+        BufferedReader reader;
+
+        try {
+            reader = new BufferedReader(new FileReader("./ids.txt"));
+            String line = reader.readLine();
+
+            while (line != null) {
+                String args[] = line.split(" ");
+                if(event.getAuthor().getId().equalsIgnoreCase(args[0])) {
+                    String[] ta = event.getMessage().getContentDisplay().split(" ");
+                    if (ta[0].equalsIgnoreCase("+ta")) {
+                        postMessage(event,args[1]);
+                        break;
+                    }
+                }else {
+                    line = reader.readLine();
                 }
             }
-        }else if(event.getAuthor().getId().equals("174739297939750912")){ //Lieutenant
-            String[] ta = event.getMessage().getContentDisplay().split(" ");
-            if(ta[0].equalsIgnoreCase("+ta")){
-                List<Message.Attachment> image = event.getMessage().getAttachments();
-                if(!image.isEmpty()){
-                    String messageID = event.getMessageId();
-                    CompletableFuture<File> future = image.get(0).downloadToFile("taa.png");
-                    future.thenAccept(file -> {
-                        event.getJDA().getGuildById("1066874960779431976").getTextChannelById("1074142744399847504")
-                                .sendMessage(event.getMessage().getContentDisplay()).addFile(new File("taa.png")).queue();
-                    });
-                    future.exceptionally(error -> { // handle possible errors
-                        error.printStackTrace();
-                        return null;
-                    });
 
-                }else{
-                    event.getJDA().getGuildById("1066874960779431976").getTextChannelById("1074142744399847504")
-                            .sendMessage(event.getMessage().getContentDisplay()).queue();
-                }
-            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+    public void postMessage(MessageReceivedEvent event, String textChannel){
+
+        List<Message.Attachment> image = event.getMessage().getAttachments();
+        if(!image.isEmpty()){
+            CompletableFuture<File> future = image.get(0).downloadToFile( "ta.png");
+            future.thenAccept(file -> {
+                event.getJDA().getGuildById(guild).getTextChannelById(textChannel)
+                        .sendMessage(event.getMessage().getContentDisplay()).addFile(new File("ta.png")).queue();
+            });
+            future.exceptionally(error -> { // handle possible errors
+                error.printStackTrace();
+                return null;
+            });
+
+        }else{
+            event.getJDA().getGuildById(guild).getTextChannelById(textChannel)
+                    .sendMessage(event.getMessage().getContentDisplay()).queue();
+        }
+
     }
 
 
